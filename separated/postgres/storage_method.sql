fbebbc141c96b22dd283b2a074e6fec25686c653
Ano Tisam <an0tis@gmail.com>
2016-08-30 11:19:35 +1100

    Reorganised folders and added Postgres SQL dumps
    Re-organised folders
    Dumped Master Data from Postgres

---
+++ separated/postgres/storage_method.sql
@@ -0,0 +1,9 @@
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (1, 'Brine (CaCl)', 'CB', NULL, 'Y');
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (2, 'Brine (NaCl)', 'BR', NULL, 'Y');
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (3, 'Circulation', 'CR', 'Y', 'Y');
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (4, 'Freezer/Air (Blast)', 'RC', NULL, 'Y');
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (5, 'Freezer/Air (Coils)', 'BF', NULL, 'Y');
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (6, 'Ice', 'IC', NULL, 'Y');
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (7, 'Natural', 'NN', 'Y', 'Y');
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (8, 'Refigerated', 'RC', 'Y', 'Y');
+INSERT INTO "storage_method" ("ID", "Storage_Methods", "Storage_Code", "Bait", "Enabled") VALUES (9, 'Refrigerated Sea Water', 'RW', NULL, 'Y');
